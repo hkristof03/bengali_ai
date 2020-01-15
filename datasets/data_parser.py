4edfe90de1bc4b1adc2e87f0aded189bff965564
hkristof03 <hkristof03@hotmail.com>
2020-01-15 18:12:26 +0100

    Modified errors.

--- datasets/data_parser.py
+++ datasets/data_parser.py
@@ -23,10 +23,10 @@ def process_parquet_files(train=True, nprocess=10):
 
     df_parquet = pd.DataFrame()
 
-    for file in files:
+    for file in parquet_files:
         df = pd.read_parquet(file)
         df_parquet = pd.concat([df_parquet, df], axis=0)
-    
+
     df_split = np.array_split(train_df, nprocess)
 
     def save_images(df, path_save):
