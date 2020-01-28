5369d9fee12679ac7bed9c22b5aaca72a966f01c
hkristof03 <hkristof03@hotmail.com>
2020-01-28 11:44:20 +0100

    Restarting EfficientNetB4

--- run_scripts.sh
+++ run_scripts.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-python train_models.py --pyaml '/experiments/resnet50_config.yaml'
-python train_models.py --pyaml '/experiments/densenet121_config.yaml'
-python train_models.py --pyaml '/experiments/efficientnetb2_config.yaml'
-python train_models.py --pyaml '/experiments/efficientnetb3_config.yaml'
+#python train_models.py --pyaml '/experiments/resnet50_config.yaml'
+#python train_models.py --pyaml '/experiments/densenet121_config.yaml'
+#python train_models.py --pyaml '/experiments/efficientnetb2_config.yaml'
+#python train_models.py --pyaml '/experiments/efficientnetb3_config.yaml'
 python train_models.py --pyaml '/experiments/efficientnetb4_config.yaml'
