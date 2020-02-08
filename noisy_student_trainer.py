f130538eccee9d7e0f73a55340d114dd1b27eb8b
hkristof03 <hkristof03@hotmail.com>
2020-02-08 18:04:37 +0100

    Solved errors.

--- noisy_student_trainer.py
+++ noisy_student_trainer.py
@@ -1,3 +1,5 @@
+import os
+
 import numpy as np
 import pandas as pd
 from tensorflow.keras.callbacks import (ReduceLROnPlateau, ModelCheckpoint,
@@ -12,7 +14,7 @@ from models.model_zoo import build_model
 class NoisyStudentTrainer(object):
     """
     """
-    base_path = os.path.dirname(os.path.abspath(__file))
+    base_path = os.path.dirname(os.path.abspath(__file__))
 
     def __init__(self, **kwargs):
         self._config_all = config_all
@@ -57,7 +59,7 @@ class NoisyStudentTrainer(object):
         }
         self.get_callbacks()
         step_size_train = ns_tr_datagen.n / ns_tr_gen.batch_size
-        step_size-valid = ns_val_datagen.n / ns_val_datagen.batch_size
+        step_size_valid = ns_val_datagen.n / ns_val_datagen.batch_size
         model = build_model(**self._model_config)
 
         train_history = model.fit(
@@ -78,55 +80,54 @@ class NoisyStudentTrainer(object):
         filenames = holdout_datagen.filenames
         step_size_holdout = holdout_datagen.n / holdout_datagen.batch_size
 
-        else:
-            metrics_names = model.metrics_names
-            results = model.evaluate(
-                holdout_datagen, steps=step_size_holdout
-            )
-            results = [float(i) for i in results]
-            d = dict(zip(metrics_names, results))
-            scores = [
-                d['root_recall'], d['vowel_recall'], d['consonant_recall']
-            ]
-            hma_recall = float(np.average(scores, weights=[2,1,1]))
-            d['hier_macro_avg_recall'] = hma_recall
-            self._config_all['results'] = d
-            path_results = (self.base_path
-                + self._test_config['path_save_config']
-                + self._callbacks_config['experiment_name'] + '.yaml')
-            dump_dict_yaml(self._config_all, path_results)
+        metrics_names = model.metrics_names
+        results = model.evaluate(
+            holdout_datagen, steps=step_size_holdout
+        )
+        results = [float(i) for i in results]
+        d = dict(zip(metrics_names, results))
+        scores = [
+            d['root_recall'], d['vowel_recall'], d['consonant_recall']
+        ]
+        hma_recall = float(np.average(scores, weights=[2,1,1]))
+        d['hier_macro_avg_recall'] = hma_recall
+        self._config_all['results'] = d
+        path_results = (self.base_path
+            + self._test_config['path_save_config']
+            + self._callbacks_config['experiment_name'] + '.yaml')
+        dump_dict_yaml(self._config_all, path_results)
 
-            # Get arrays of predictions for later analysis
-            results = model.predict(
-                holdout_datagen, steps=step_size_holdout, verbose=1
-            )
-            root_pred = results[0]
-            vowel_pred = results[1]
-            consonant_pred = results[2]
+        # Get arrays of predictions for later analysis
+        results = model.predict(
+            holdout_datagen, steps=step_size_holdout, verbose=1
+        )
+        root_pred = results[0]
+        vowel_pred = results[1]
+        consonant_pred = results[2]
 
-            root_pred = np.argmax(i, axis=1)
-            vowel_pred = np.argmax(i, axis=1)
-            consonant_pred = np.argmax(i, axis=1)
+        root_pred = np.argmax(i, axis=1)
+        vowel_pred = np.argmax(i, axis=1)
+        consonant_pred = np.argmax(i, axis=1)
 
-            d = {
-                'image_id': filenames,
-                'root_pred': root_pred,
-                'vowel_pred': vowel_pred,
-                'consonant_pred': consonant_pred
-            }
-            df_pred = pd.DataFrame.from_dict(d)
-            df_pred = df_pred.merge(
-                self._datagen._holdout_df,
-                how='left',
-                on='image_id'
-            )
-            cols = ['grapheme_root', 'vowel_diacritic', 'consonant_diacritic']
-            for col in cols:
-                df_pred[col] = df_pred[col].apply(lambda x: np.argmax(x))
+        d = {
+            'image_id': filenames,
+            'root_pred': root_pred,
+            'vowel_pred': vowel_pred,
+            'consonant_pred': consonant_pred
+        }
+        df_pred = pd.DataFrame.from_dict(d)
+        df_pred = df_pred.merge(
+            self._datagen._holdout_df,
+            how='left',
+            on='image_id'
+        )
+        cols = ['grapheme_root', 'vowel_diacritic', 'consonant_diacritic']
+        for col in cols:
+            df_pred[col] = df_pred[col].apply(lambda x: np.argmax(x))
 
-            path_predictions = (self.base_path
-                + self._test_config['path_predictions']
-                + self._callbacks_config['experiment_name'] + '.csv')
-            df_pred.to_csv(path_predictions, index=False)
+        path_predictions = (self.base_path
+            + self._test_config['path_predictions']
+            + self._callbacks_config['experiment_name'] + '.csv')
+        df_pred.to_csv(path_predictions, index=False)
 
-            ## Returns holdout df here in order to train a last model
+        ## Returns holdout df here in order to train a last model
